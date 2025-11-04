60a023f5f15e67665c5603cc4f6986735392bf28
Dominik Tröndle <domi.troendle@gmail.com>
2025-11-04 08:09:20 +0100

    added projects and contact buttons to atf section

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  currentLanguage:string = "eng";
+
+  changeLanguage(lang:string){
+    this.currentLanguage = lang;
+  }
 
 }
