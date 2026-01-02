843c4c8db813426c77aede63b3a4acfa5c351bca
Dominik Tröndle <domi.troendle@gmail.com>
2026-01-02 08:07:04 +0100

    added OverlayService

--- src/app/services/overlay.service.ts
+++ src/app/services/overlay.service.ts
@@ -1,10 +1,36 @@
-import { Injectable } from '@angular/core';
-import { Overlay } from '@angular/cdk/overlay';
+import { Injectable, Injector } from '@angular/core';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { OverlayComponent } from '../components/overlay/overlay.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OverlayService {
 
-  constructor(private overlay: Overlay) { }
+  constructor(private overlay: Overlay, private injector: Injector) { }
+
+  open(): OverlayRef {
+    const overlayRef = this.overlay.create({
+      hasBackdrop: true,
+      panelClass: 'overlay-panel',
+      backdropClass: 'overlay-backdrop',
+      scrollStrategy: this.overlay.scrollStrategies.block(),
+      positionStrategy: this.overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .centerVertically()
+    });
+    const injector = Injector.create({
+      providers: [{ provide: OverlayRef, useValue: overlayRef }]
+    });
+    overlayRef.attach(
+      new ComponentPortal(OverlayComponent, null, injector)
+    );
+    overlayRef.backdropClick().subscribe(() => {
+      overlayRef.dispose();
+    });
+    return overlayRef;
+  }
 }
