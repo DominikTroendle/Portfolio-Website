1eeb468b190d6e2e73f06b729754900a47fd49c6
Dominik Tröndle <domi.troendle@gmail.com>
2026-01-08 11:35:15 +0100

    adjusted carousel card content to be rendered from object

--- src/app/components/references/references.component.ts
+++ src/app/components/references/references.component.ts
@@ -9,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class ReferencesComponent {
   active:boolean = false;
+
+  carouselData = [
+    {
+      name: "Nico Reckert",
+      role: "Team Partner Project Join",
+      comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+    },
+    {
+      name: "Kevin Breiter",
+      role: "Team Partner Project Join",
+      comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore."
+    },
+    {
+      name: "kjkjasdl ölowo-öäla",
+      role: "lkllakkddds",
+      comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr."
+    },
+  ]
 }
