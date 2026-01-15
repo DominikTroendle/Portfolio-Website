c71a4d8e35dce527e0f52d984e49f981af488ad3
Dominik Tröndle <domi.troendle@gmail.com>
2026-01-15 09:25:56 +0100

    installed firebase-tools

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -11,8 +11,10 @@ import { CommonModule } from '@angular/common';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
-  currentLanguage:string = "eng";
+  // headerData und headerHref werden dynamisch befüllt, abhängig von currentLanguage (also headerData = this.databaseService[curentLanguage].headerData)
+  headerData = ["About me", "Skills", "Projects"];
+  headerHref = ["#about", "#skills", "#projects"];
+  currentLanguage:string = "english";
 
   changeLanguage(lang:string){
     this.currentLanguage = lang;
