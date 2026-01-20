5809b02f731a89820bab9107783a3e49d64c09f4
Dominik Tröndle <domi.troendle@gmail.com>
2026-01-20 11:11:13 +0100

    added function to cacle through projects and update overlay values according to the nmext selected project

--- src/app/components/overlay/overlay.component.ts
+++ src/app/components/overlay/overlay.component.ts
@@ -11,11 +11,17 @@ import { DatabaseService } from '../../services/database.service';
 export class OverlayComponent {
 
   @Input()overlayData: any;
+  @Input()projectNumber: any;
   @Output()close = new EventEmitter<Event>();
+  @Output()next = new EventEmitter<Event>();
 
   constructor(public db: DatabaseService) { }
 
   closeOverlay(){
     this.close.emit();
+  }
+
+  nextProject(){
+    this.next.emit();
   }
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -17,16 +17,27 @@ export class ProjectsComponent {
 
   isVisible = false;
   selectedProject = {};
+  selectedIndex = 0;
 
   constructor(public db: DatabaseService) { }
 
-  handleOverlay(index: number){
-    this.isVisible = !this.isVisible;
+  showOverlay(index: number){
+    this.isVisible = true;
     this.selectedProject = this.db.data.projectsData.overlay[index];
+    this.selectedIndex = index + 1;
+  }
+
+  updateOverlay(displayIndex: number){
+    const length = this.db.data.projectsData.overlay.length;
+    const currentIndex = displayIndex -1;
+    const nextIndex = (currentIndex + 1) % length;
+    this.selectedProject = this.db.data.projectsData.overlay[nextIndex];
+    this.selectedIndex = nextIndex + 1;
   }
 
   closeOverlay(){
     this.isVisible = false;
+    this.selectedIndex = 0;
   }
 
 }
