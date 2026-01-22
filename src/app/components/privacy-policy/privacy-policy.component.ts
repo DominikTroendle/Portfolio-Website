5d18f42378866514cd722468ac31836c4c50c1d3
Dominik Tröndle <domi.troendle@gmail.com>
2026-01-22 11:02:04 +0100

    added privacy policy component and styling

---
+++ src/app/components/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DatabaseService } from '../../services/database.service';
+
+@Component({
+  selector: 'app-privacy-policy',
+  standalone: true,
+  imports: [],
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.scss'
+})
+export class PrivacyPolicyComponent {
+  constructor(public db: DatabaseService) { }
+}
