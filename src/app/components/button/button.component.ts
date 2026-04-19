a2eddfef19fcca5059fa3def40c672e7b96f4a1d
Dominik Tröndle <domi.troendle@gmail.com>
2026-04-19 14:40:43 +0200

    started refactoring semantic structure, adjusted button component with routerLink and fragments

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
+
 export class ButtonComponent {
-  @Input()text:any;
+  @Input() text: string = '';
+  @Input() fragment?: string;
+  @Input() route: string = '/';
 }
