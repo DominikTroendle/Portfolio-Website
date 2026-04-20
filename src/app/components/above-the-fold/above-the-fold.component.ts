f22c1949074f9dd5c10bec924ab8ae1a80545603
Dominik Tröndle <domi.troendle@gmail.com>
2026-04-20 08:07:29 +0200

    refactored class names and scss structure after BEM conventions, refactored semantic structure

--- src/app/components/above-the-fold/above-the-fold.component.ts
+++ src/app/components/above-the-fold/above-the-fold.component.ts
@@ -8,26 +8,24 @@ import {
 import { DatabaseService } from '../../services/database/database.service';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ButtonComponent } from "../button/button.component";
+import { ButtonComponent } from '../button/button.component';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [
-    ButtonComponent,
-    RouterLink
-  ],
+  imports: [ButtonComponent, RouterLink],
   templateUrl: './above-the-fold.component.html',
-  styleUrl: './above-the-fold.component.scss'
+  styleUrl: './above-the-fold.component.scss',
 })
-export class AboveTheFoldComponent implements AfterViewInit, OnDestroy {
-  constructor(public db: DatabaseService) { }
 
-  @ViewChild('hero') hero!: ElementRef;
+export class AboveTheFoldComponent implements AfterViewInit, OnDestroy {
+  constructor(public db: DatabaseService) {}
+
+  @ViewChild('atf') atf!: ElementRef;
   @ViewChild('main') main!: ElementRef;
-  @ViewChild('sideLeft') sideLeft!: ElementRef;
-  @ViewChild('sideRight') sideRight!: ElementRef;
+  @ViewChild('left') left!: ElementRef;
+  @ViewChild('right') right!: ElementRef;
   @ViewChild('ribbon') ribbon!: ElementRef;
 
   private ctx!: gsap.Context;
@@ -39,31 +37,31 @@ export class AboveTheFoldComponent implements AfterViewInit, OnDestroy {
         yPercent: -100,
         ease: 'none',
         scrollTrigger: {
-          trigger: this.hero.nativeElement,
+          trigger: this.atf.nativeElement,
           start: 'top top',
           end: '+=100%',
-          scrub: 0.8
-        }
+          scrub: 0.8,
+        },
       });
-      gsap.to(this.sideLeft.nativeElement, {
+      gsap.to(this.left.nativeElement, {
         yPercent: -100,
         ease: 'none',
         scrollTrigger: {
-          trigger: this.hero.nativeElement,
+          trigger: this.atf.nativeElement,
           start: 'top top',
           end: '+=100%',
-          scrub: 0.8
-        }
+          scrub: 0.8,
+        },
       });
-      gsap.to(this.sideRight.nativeElement, {
+      gsap.to(this.right.nativeElement, {
         yPercent: -100,
         ease: 'none',
         scrollTrigger: {
-          trigger: this.hero.nativeElement,
+          trigger: this.atf.nativeElement,
           start: 'top top',
           end: '+=100%',
-          scrub: 0.8
-        }
+          scrub: 0.8,
+        },
       });
       gsap.fromTo(
         this.ribbon.nativeElement,
@@ -72,12 +70,12 @@ export class AboveTheFoldComponent implements AfterViewInit, OnDestroy {
           y: 150,
           opacity: 0,
           scrollTrigger: {
-            trigger: this.hero.nativeElement,
+            trigger: this.atf.nativeElement,
             start: 'top top',
             end: '+=100%',
-            scrub: 0.8
-          }
-        }
+            scrub: 0.8,
+          },
+        },
       );
     });
     ScrollTrigger.refresh();
