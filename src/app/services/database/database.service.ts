debe2edfe92f167e4c9f8de244ee55e2b39ccdca
Dominik Tröndle <domi.troendle@gmail.com>
2026-04-28 19:25:10 +0200

    adjusted loadItems function to correctly type doc.data, outsourced type assignments to assignData function

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -1,7 +1,18 @@
 import { inject, Injectable } from '@angular/core';
-import { collection, Firestore, getDocs } from '@angular/fire/firestore';
+import { collection, DocumentData, Firestore, getDocs } from '@angular/fire/firestore';
 import { LocalStorageService } from '../localStorage/local-storage.service';
-import { HeaderData, FooterData, AboutData, AtfData, SkillsData, LegalData, PrivacyData, ContactData, ReferencesData } from './database.types';
+import {
+  HeaderData,
+  FooterData,
+  AboutData,
+  AtfData,
+  SkillsData,
+  LegalData,
+  PrivacyData,
+  ContactData,
+  ReferencesData,
+  ProjectsData
+} from './database.types';
 
 type AppData = {
   aboutData?: AboutData;
@@ -11,31 +22,68 @@ type AppData = {
   headerData?: HeaderData;
   legalNoticeData?: LegalData;
   privacyPolicyData?: PrivacyData;
-  projectsData?: any;
+  projectsData?: ProjectsData;
   referencesData?: ReferencesData;
   skillsData?: SkillsData;
-}
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class DatabaseService {
-  currentLanguage: "english" | "german" = "english";
+  currentLanguage: 'english' | 'german' = 'english';
   data: AppData = {};
   firestore: Firestore = inject(Firestore);
   loaded = false;
-  
-  constructor(private localStorage: LocalStorageService) { }
 
-  async loadItems() {
-    const storedLanguage = this.localStorage.get("selectedLanguage");
-    if(storedLanguage) this.currentLanguage = storedLanguage;
-    const dataRef = await getDocs(collection(this.firestore, this.currentLanguage));
-    dataRef.forEach(doc => {
+  constructor(private localStorage: LocalStorageService) {}
+
+  async loadItems(): Promise<void> {
+    const storedLanguage = this.localStorage.get('selectedLanguage');
+    if (storedLanguage) this.currentLanguage = storedLanguage;
+    const dataRef = await getDocs(
+      collection(this.firestore, this.currentLanguage),
+    );
+    dataRef.forEach((doc) => {
       const key = doc.id as keyof AppData;
-      this.data[key] = doc.data();
+      this.assignData(key, doc.data())
     });
     this.loaded = true;
+  }
+
+  private assignData(key: keyof AppData, value: DocumentData): void {
+    switch (key) {
+      case 'aboutData':
+        this.data.aboutData = value as AboutData;
+        break;
+      case 'atfData':
+        this.data.atfData = value as AtfData;
+        break;
+      case 'contactData':
+        this.data.contactData = value as ContactData;
+        break;
+      case 'footerData':
+        this.data.footerData = value as FooterData;
+        break;
+      case 'headerData':
+        this.data.headerData = value as HeaderData;
+        break;
+      case 'legalNoticeData':
+        this.data.legalNoticeData = value as LegalData;
+        break;
+      case 'privacyPolicyData':
+        this.data.privacyPolicyData = value as PrivacyData;
+        break;
+      case 'projectsData':
+        this.data.projectsData = value as ProjectsData;
+        break;
+      case 'referencesData':
+        this.data.referencesData = value as ReferencesData;
+        break;
+      case 'skillsData':
+        this.data.skillsData = value as SkillsData;
+        break;
+    }
   }
 }
